7143b8bdae022b7ed50cbda9112283fae3d39ef1
Nathan Cox <nrc4@hotmail.com>
2025-04-08 15:51:18 -0700

    Update the loading.tsx file to pause for half a second before rendering; add a layout.tsx in the tours directory.

--- react-next-ts/demo-apps/nextjs-tutorial/app/loading.tsx
+++ react-next-ts/demo-apps/nextjs-tutorial/app/loading.tsx
@@ -1,7 +1,20 @@
 'use client';
 
-import { GridLoader } from 'react-spinners';
+import { GridLoader } from "react-spinners"
+import { useState, useEffect } from 'react';
 
-export default function Loading() {
+export default function Loading(): React.ReactNode {
+  const [delayed, setDelayed] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDelayed(true);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!delayed) return null;
+
   return <GridLoader color='orange' />
 }
