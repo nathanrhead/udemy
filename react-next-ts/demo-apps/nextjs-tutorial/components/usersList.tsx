ac1ed025f96833b186d5e5487c812bd121b40aad
Nathan Cox <nrc4@hotmail.com>
2025-04-09 11:56:07 -0700

    Remove console logs.

--- react-next-ts/demo-apps/nextjs-tutorial/components/usersList.tsx
+++ react-next-ts/demo-apps/nextjs-tutorial/components/usersList.tsx
@@ -3,8 +3,6 @@ import { fetchUsers, User } from '../utils/actions'
 export default async function UsersList() {
   const users = await fetchUsers()
 
-  console.log({users})
-
   return (
     <div className="flex flex-col items-center min-h-screen">
       <h1 className="text-2xl font-bold mb-4">Users List</h1>
